78eca291417a90bdee6dcc10633fd612d209713f
Carlos Doki <carlosdoki@Doki-Econocom-MacBookPro.local>
2017-03-13 13:44:29 -0300

    First commit

---
+++ TacoPOP/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  MainVC
+//  TacoPOP
+//
+//  Created by Carlos Doki on 3/13/17.
+//  Copyright © 2017 Carlos Doki. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
